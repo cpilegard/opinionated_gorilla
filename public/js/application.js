d115a3953c4b9264489f4da5578113852405bbdd
jesseclay <jesseclay@gmail.com>
2013-07-26 12:32:57 -0700

    basic index page now working with login link

--- public/js/application.js
+++ public/js/application.js
@@ -4,4 +4,11 @@ $(document).ready(function() {
   // when we try to bind to them
 
   // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  $('.nav li').mouseover(function () {
+    $(this).addClass('active');
+  });
+
+  $('.nav li').mouseleave(function() {
+    $('li').removeClass('active');
+  });
 });
