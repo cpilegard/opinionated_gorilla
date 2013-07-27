171bee2270bb94693f4cbff3f71e3d689d3c7541
Cole Pilegard <c.pilegard@gmail.com>
2013-07-26 19:19:11 -0700

    added radio buttons for take_survey

--- app/controllers/take_survey.rb
+++ app/controllers/take_survey.rb
@@ -1,16 +1,8 @@
 get '/survey/:id/take' do
   @survey = Survey.find_by_id(params[:id])
-  # questions = survey.questions
-  # @questions_and_choices = [[]]
-  # questions.each_with_index do |question, i|
-  #   p "question is #{question}"
-  #   p "index is #{i}"
-  #   @questions_and_choices[i] = question
-  #   question.choices.each_with_index do |choice, j|
-  #     @questions_and_choices[i][j] = choice
-  #   end
-  # end
-  # p @questions_and_choices
-
   erb :take_survey
+end
+
+post '/survey/:id/submit' do
+  
 end
