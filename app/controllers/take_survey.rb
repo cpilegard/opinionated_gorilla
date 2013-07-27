278aacf109c21c1c6490689774a7c67e94ee89e1
Cole Pilegard <c.pilegard@gmail.com>
2013-07-26 19:33:00 -0700

    users can take survey, choices logged in user_choices

--- app/controllers/take_survey.rb
+++ app/controllers/take_survey.rb
@@ -4,5 +4,12 @@ get '/survey/:id/take' do
 end
 
 post '/survey/:id/submit' do
-  
+  survey = Survey.find(params[:id])
+
+  survey.questions.each do |question|
+    chosen = Choice.find(params["#{question.id}"].to_i)
+    UserChoice.create(user: current_user, choice: chosen)
+  end
+
+  redirect '/'
 end
