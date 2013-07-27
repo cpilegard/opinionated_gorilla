9ffefb369c40f4ef219d3ceec62af496a5b3622e
Cole Pilegard <c.pilegard@gmail.com>
2013-07-27 15:53:00 -0700

    added support for thumbnails, shitty styling

--- app/uploaders/uploader.rb
+++ app/uploaders/uploader.rb
@@ -1,6 +1,10 @@
 class Uploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
   process :resize_to_fill => [200, 200]
+
+  version :thumb do
+    process :resize_to_fill => [80, 80]
+  end
   
   storage :file
 
